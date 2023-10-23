c4af24a14292635819cf859fd3c30de6ae66486c
sarumani <qkre2018@gmail.com>
2023-10-23 23:37:03 +0900

    1206. [S/W 문제해결 기본] 1일차 - View

---
+++ PJH/1206.py
@@ -0,0 +1,39 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N = int(input())
+
+    buildings = list(map(int, input().split()))
+
+    for i in range(N):
+        left = False
+        right = False
+
+        view = 0
+
+        if i == 0:
+            left = True
+
+        elif i == 1:
+            if buildings[i - 1] < buildings[i]:
+                left = True
+        else:
+            if buildings[i - 2] < buildings[i] and buildings[i - 1] < buildings[i]:
+                left = True
+
+        if i == N - 1:
+            right = True
+        elif i == N - 2:
+            if buildings[i + 1] < buildings[i]:
+                right = True
+        else:
+            if buildings[i + 2] < buildings[i] and buildings[i + 1] < buildings[i]:
+                right = True
+
+        if left and right:
+            if i == 0:
+                view += buildings[i] - max(buildings[: i + 3])
