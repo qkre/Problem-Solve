b891a1ae340a8fdb075187339252737d6fe824f6
sarumani <qkre2018@gmail.com>
2023-10-24 17:12:08 +0900

    1244. 최대 상금... 답지 봄

--- PJH/1244.py
+++ PJH/1244.py
@@ -2,49 +2,33 @@ import sys
 
 sys.stdin = open("input/1244_input.txt", "r")
 
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    numbers, cnt = input().split()
-    numbers = list(numbers)
-    cnt = int(cnt)
 
-    if numbers == sorted(numbers, reverse=True):
-        for i in range(cnt):
-            tmp = numbers[-2]
-            numbers[-2] = numbers[-1]
-            numbers[-1] = tmp
-    else:
-        swapped = 0
-        index = 0
+def dfs(index, count):
+    global result
+    if count == int(cnt):
+        result = max(int("".join(data)), result)
+        return
+    for now in range(index, len(data)):
+        for max_idx in range(now + 1, len(data)):
+            if data[now] <= data[max_idx]:
+                data[now], data[max_idx] = data[max_idx], data[now]
+                dfs(now, count + 1)
+                data[now], data[max_idx] = data[max_idx], data[now]
 
-        reverse_sorted_numbers = sorted(numbers, reverse=True)
+    if result == 0 and count < int(cnt):
+        extra = (int(cnt) - count) % 2
+        # 짝수라면 그대로, 홀수라면 한 번 변경
+        if extra == 1:  # 홀수라면
+            data[-1], data[-2] = data[-2], data[-1]
+        dfs(index, int(cnt))
 
-        while swapped != cnt:
-            if numbers[index] < max(numbers[index + 1 :]):
-                tmp = numbers[index]
-                reversed_numbers = list(reversed(numbers[index + 1 :]))
 
-                max_idx = (
-                    len(numbers) - 1 - reversed_numbers.index(max(numbers[index + 1 :]))
-                )
+t = int(input())
 
-                numbers[index] = numbers[max_idx]
-                numbers[max_idx] = tmp
-                swapped += 1
-                index += 1
+for tc in range(1, t + 1):
+    data, cnt = input().split()
+    data = list(data)
+    result = 0
+    dfs(0, 0)
 
-            elif numbers == reverse_sorted_numbers:
-                for j in range(index, cnt):
-                    tmp = numbers[-2]
-                    numbers[-2] = numbers[-1]
-                    numbers[-1] = tmp
-                    swapped += 1
-                    if swapped == cnt:
-                        break
-            else:
-                index += 1
-
-    print(f"#{test_case} {''.join(numbers)}")
-
-# 아무래도 뒤에서 부터 스왑 하는 것이 정답일 듯 .....
+    print("#%d %d" % (tc, result))
