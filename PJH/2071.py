7ba2662f9fc61f552c941574447ca02a241ad1d6
sarumani <qkre2018@gmail.com>
2023-10-25 16:18:47 +0900

    반올림 : round()

--- PJH/2071.py
+++ PJH/2071.py
@@ -1,7 +1,10 @@
+import sys
+
+sys.stdin = open("input/2071_input.txt", "r")
+
 T = int(input())
 
 for case in range(1, T + 1):
-    N = int(input())
     scores = list(map(int, input().split()))
 
     average = 0
@@ -9,6 +12,6 @@ for case in range(1, T + 1):
     for i in scores:
         average += i
 
-    average /= N
+    average /= len(scores)
 
-    print(f"#{case} {average}")
+    print(f"#{case} {round(average)}")
