a1a6af6576ab6acaf3a3b418c3f11da738a7c52f
sarumani <qkre2018@gmail.com>
2023-10-25 17:34:38 +0900

    1928. Base64 Decoder

--- PJH/1928.py
+++ PJH/1928.py
@@ -12,7 +12,24 @@ T = int(input())
 
 for case in range(1, T + 1):
     encoded = list(input())
+    binary_encoded = []
+
+    print(f"#{case} ", end="")
 
     for i in encoded:
         binary = format(encoding_table.index(i), "b")
-        print(binary)
+        for i in list(binary.zfill(6)):
+            binary_encoded.append(i)
+
+    binary_decoded = []
+
+    for i in range(0, len(binary_encoded), 8):
+        decoded = "".join(binary_encoded[i : i + 8])
+        decoded = "0b" + decoded
+        binary_decoded.append(decoded)
+
+    for binary in binary_decoded:
+        ascii_code = int(binary, 2)
+        print(chr(ascii_code), end="")
+
+    print()
