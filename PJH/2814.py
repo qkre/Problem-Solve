afa77bfa95dd2dcd1be9d53ec39a5a40adebe482
sarumani <qkre2018@gmail.com>
2023-10-28 19:24:20 +0900

    2814. 최장 경로

--- PJH/2814.py
+++ PJH/2814.py
@@ -8,13 +8,43 @@ result = []
 
 T = int(input())
 
+def dfs(s):
+    global N, length
+
+    length = max(length, visited.count(True))
+    
+    for i in graph[s]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(i)
+
+            visited[i] = False
+
+        
+
 for case in range(1, T + 1):
 
     N, M = map(int, input().split())
 
-    
-    
+    graph = [[] for _ in range(N)]
+    visited = [False] * N
+    length = 1
 
+    for _ in range(M):
+        S, E = map(int, input().split())
+        graph[S-1].append(E-1)
+        graph[E-1].append(S-1)
+    
+    if M > 0:
+        for i in range(N):
+            for j in graph[i]:
+                visited[i] = True
+                visited[j] = True
+                dfs(j)
+                visited[i] = False
+                visited[j] = False
+
+    result.append(f"#{case} {length}")
 for _ in result:
     print(_)
 
