dd9ecb3ae2e76db67ff83481349aa73f0c623b81
sarumani <qkre2018@gmail.com>
2023-10-30 20:33:55 +0900

    1976. 시각 덧셈

---
+++ PJH/1976.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+
+current_file = os.path.basename(__file__)[:-3]
+sys.stdin = open(f"input/{current_file}_input.txt", "r")
+
+
+result = []
+
+T = int(input())
+
+for case in range(1, T + 1):
+    t1, m1, t2, m2 = map(int, input().split())
+
+    rt = t1 + t2
+    rm = m1 + m2
+
+    if rm >= 60:
+        rt += rm // 60
+        rm -= 60 * (rm // 60)
+
+    if rt > 12:
+        rt -= 12
+
+    result.append(f"#{case} {rt} {rm}")
+
+
+for _ in result:
+    print(_)
+
+
+output = open(f"input/{current_file}_output.txt", "r").readlines()
+output = [line.strip() for line in output]
+
+print("------------------- 오답 ------------------ ( 이 아래로 출력이 없으면 정답)")
+
+for r, o in zip(result, output):
+    if r != o:
+        print(f"정답 : {o},     오답 : {r}")
