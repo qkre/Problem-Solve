49197e0c3c75c6b70e4f97dd3dc14e6e73dfe3e4
sarumani <qkre2018@gmail.com>
2023-10-30 20:21:38 +0900

    1284. 수도 요금 경쟁

---
+++ PJH/1284.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+current_file = os.path.basename(__file__)[:-3]
+sys.stdin = open(f"input/{current_file}_input.txt", "r")
+
+
+result = []
+
+T = int(input())
+
+for case in range(1, T + 1):
+    P, Q, R, S, W = map(int, input().split())
+
+    A = P * W
+    B = Q + S * (W - R) if W >= R else Q
+
+    result.append(f"#{case} {min(A, B)}")
+
+for _ in result:
+    print(_)
+
+
+output = open(f"input/{current_file}_output.txt", "r").readlines()
+output = [line.strip() for line in output]
+
+print("------------------- 오답 ------------------ ( 이 아래로 출력이 없으면 정답)")
+
+for r, o in zip(result, output):
+    if r != o:
+        print(f"정답 : {o},     오답 : {r}")
