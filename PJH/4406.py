6b6af13279f16c56a398ce9661c709f8400cf14f
sarumani <qkre2018@gmail.com>
2023-11-02 17:40:17 +0900

    4406. 모음이 보이지 않는 사람 D3

---
+++ PJH/4406.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+
+current_file = os.path.basename(__file__)[:-3]
+sys.stdin = open(f"input/{current_file}_input.txt", "r", encoding="utf-8-sig")
+
+
+result = []
+
+T = int(input())
+
+for case in range(1, T + 1):
+    s = list(input())
+    ans = []
+    i = 0
+
+    for i in s:
+        if not i in ["a", "e", "i", "o", "u"]:
+            ans.append(i)
+
+    result.append(f"#{case} {''.join(ans)}")
+
+for _ in result:
+    print(_)
+
+
+output = open(f"input/{current_file}_output.txt", "r").readlines()
+output = [line.strip() for line in output]
+
+print("------------------- 오답 ------------------ ( 이 아래로 출력이 없으면 정답)")
+
+for r, o in zip(result, output):
+    if r != o:
+        print(f"정답 : {o},     오답 : {r}")
