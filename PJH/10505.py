23a0f39c77abbb0639fbd149006d77ca395b4a52
sarumani <qkre2018@gmail.com>
2023-11-02 17:30:45 +0900

    10505. 소득 불균형 D3

---
+++ PJH/10505.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+
+current_file = os.path.basename(__file__)[:-3]
+sys.stdin = open(f"input/{current_file}_input.txt", "r", encoding="utf-8-sig")
+
+
+result = []
+
+T = int(input())
+
+for case in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    avg = sum(arr) / N
+
+    cnt = 0
+    for i in arr:
+        if avg >= i:
+            cnt += 1
+
+    result.append(f"#{case} {cnt}")
+
+
+for _ in result:
+    print(_)
+
+
+output = open(f"input/{current_file}_output.txt", "r").readlines()
+output = [line.strip() for line in output]
+
+print("------------------- 오답 ------------------ ( 이 아래로 출력이 없으면 정답)")
+
+for r, o in zip(result, output):
+    if r != o:
+        print(f"정답 : {o},     오답 : {r}")
