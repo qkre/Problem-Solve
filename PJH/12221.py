400baf6abcb3573c13f9a77641f546ec294e26f9
sarumani <qkre2018@gmail.com>
2023-11-02 17:46:28 +0900

    12221. 구구단2 D3

---
+++ PJH/12221.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+
+current_file = os.path.basename(__file__)[:-3]
+sys.stdin = open(f"input/{current_file}_input.txt", "r", encoding="utf-8-sig")
+
+
+result = []
+
+T = int(input())
+
+for case in range(1, T + 1):
+    A, B = map(int, input().split())
+
+    if A >= 10 or B >= 10:
+        result.append(f"#{case} -1")
+    else:
+        result.append(f"#{case} {A*B}")
+
+
+for _ in result:
+    print(_)
+
+
+output = open(f"input/{current_file}_output.txt", "r").readlines()
+output = [line.strip() for line in output]
+
+print("------------------- 오답 ------------------ ( 이 아래로 출력이 없으면 정답)")
+
+for r, o in zip(result, output):
+    if r != o:
+        print(f"정답 : {o},     오답 : {r}")
