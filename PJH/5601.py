f1b34a29e78a0b7642568022da70f2e497fafe19
sarumani <qkre2018@gmail.com>
2023-11-04 15:44:51 +0900

    5601. [Professional] 쥬스 나누기 D3

---
+++ PJH/5601.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+
+current_file = os.path.basename(__file__)[:-3]
+sys.stdin = open(f"input/{current_file}_input.txt", "r", encoding="utf-8-sig")
+
+
+result = []
+
+T = int(input())
+
+for case in range(1, T + 1):
+    N = int(input())
+
+    ans = ""
+
+    for _ in range(N):
+        ans += f"1/{N} "
+
+    result.append(f"#{case} {ans[:-1]}")
+
+for _ in result:
+    print(_)
+
+
+output = open(f"input/{current_file}_output.txt", "r").readlines()
+output = [line.strip() for line in output]
+
+print("------------------- 오답 ------------------ ( 이 아래로 출력이 없으면 정답)")
+
+for r, o in zip(result, output):
+    if r != o:
+        print(f"정답 : {o},     오답 : {r}")
