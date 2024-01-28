26e4b36f0d8388fb06b7bec3adcc1dde00aeb552
sarumani <qkre2018@gmail.com>
2024-01-28 18:08:50 +0900

    같은 숫자는 싫어

---
+++ PJH/programmers/12906.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+
+    for i in arr:
+        if len(answer) == 0:
+            answer.append(i)
+
+        elif answer[-1] != i:
+            answer.append(i)
+
+    return answer
+
+print(solution([4,4,4,3,3]))
