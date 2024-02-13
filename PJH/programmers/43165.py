7fa48facb2523ee92374d628f7c017ed12cfbd44
sarumani <qkre2018@gmail.com>
2024-02-13 09:19:41 +0900

    타겟 넘버

--- PJH/programmers/43165.py
+++ PJH/programmers/43165.py
@@ -1,20 +1,22 @@
 answer = 0
 
+def dfs(value, depth, numbers, target, length):
+    global answer
+
+    if depth == length:
+        if target == value:
+            answer += 1
+        return
+
+    dfs(value + numbers[depth], depth + 1, numbers, target, length)
+
+    dfs(value - numbers[depth], depth + 1, numbers, target, length)
 
 def solution(numbers, target):
     global answer
-    def dfs(value, depth):
-        global answer
-        if depth == len(numbers):
-            if target == value:
-                answer += 1
-            return
-
-        dfs(value + numbers[depth], depth+1)
-        dfs(value - numbers[depth], depth+1)
 
 
-    dfs(0, 0)
+    dfs(0, 0, numbers, target, len(numbers))
 
     return answer
 
