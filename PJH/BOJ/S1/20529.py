be791301461f872f3a5fe5a5ebc280287121769d
sarumani <qkre2018@gmail.com>
2024-02-20 16:29:03 +0900

    가장 가까운 세사람의 심리적 거리

--- PJH/BOJ/S1/20529.py
+++ PJH/BOJ/S1/20529.py
@@ -1,32 +1,47 @@
 from sys import stdin
+from collections import deque
+
 input = stdin.readline
+
 
 def solution():
     T = int(input())
     for _ in range(T):
+        answer = 2e9
         N = int(input())
         arr = list(input().split())
-
-        graph = [[0 for _ in range(N)] for _ in range(N)]
-
-        for i in range(N):
-            for j in range(i+1, N):
-
-                for k in range(4):
-                    if arr[i][k] != arr[j][k]:
-                        graph[i][j] += 1
-                        graph[j][i] += 1
+        if len(arr) >= 48:
+            print(0)
+            continue
 
         for i in range(N):
-            for j in range(i+1, N):
-                q = (i, j)
+            for j in range(i + 1, N):
+                for k in range(j + 1, N):
+                    A = arr[i]
+                    B = arr[j]
+                    C = arr[k]
 
-                visited = [False] * N
-                visited[j] = True
+                    distance_AB = 0
+                    distance_BC = 0
+                    distance_AC = 0
 
-                while q:
-                    s, e = q.popleft()
+                    if A != B:
+                        for l in range(4):
+                            if A[l] != B[l]:
+                                distance_AB += 1
+                    if B != C:
+                        for l in range(4):
+                            if B[l] != C[l]:
+                                distance_BC += 1
 
+                    if A != C:
+                        for l in range(4):
+                            if A[l] != C[l]:
+                                distance_AC += 1
+
+                    answer = min(answer, distance_AB + distance_BC + distance_AC)
+
+        print(answer)
 
 
 solution()
