dcfd7fe19fed5f5167844d757f0e8642cec6bcef
sarumani <qkre2018@gmail.com>
2024-03-06 00:25:36 +0900

    행렬 제곱

--- PJH/BOJ/G4/10830.py
+++ PJH/BOJ/G4/10830.py
@@ -3,55 +3,34 @@ from copy import deepcopy
 
 input = stdin.readline
 
+def times(arr, target, N):
+
+    answer = [[0 for _ in range(N)] for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                answer[i][j] += (arr[i][k] % 1000 * target[k][j] % 1000) % 1000
+
+    return answer
 
 def solution():
     N, B = map(int, input().split())
 
     arr = [list(map(int, input().split())) for _ in range(N)]
     org = deepcopy(arr)
-    n = deepcopy(arr)
 
     bin_B = format(B, 'b')
 
 
     for case in range(1, len(bin_B)):
         if bin_B[case] == '1':
-            for i in range(N):
-                for j in range(N):
-                    temp = 0
-                    for k in range(N):
-                        temp += (arr[i][k] % 1000 * arr[k][j] % 1000) % 1000
-
-                    n[i][j] = temp % 1000
-
-            for i in range(N):
-                for j in range(N):
-                    arr[i][j] = n[i][j]
-
-            for i in range(N):
-                for j in range(N):
-                    temp = 0
-                    for k in range(N):
-                        temp += (arr[i][k] % 1000 * org[k][j] % 1000) % 1000
-
-                    n[i][j] = temp % 1000
-
-            for i in range(N):
-                for j in range(N):
-                    arr[i][j] = n[i][j]
+            arr = times(arr, arr, N)
+            arr = times(arr, org, N)
 
         else:
-            for i in range(N):
-                for j in range(N):
-                    temp = 0
-                    for k in range(N):
-                        temp += (arr[i][k] % 1000 * arr[k][j] % 1000) % 1000
+            arr = times(arr, arr, N)
 
-                    n[i][j] = temp % 1000
-
-            for i in range(N):
-                for j in range(N):
-                    arr[i][j] = n[i][j]
 
     for i in range(N):
         for j in range(N):
